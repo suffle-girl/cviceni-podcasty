51d89f38096428aa2a642abe4138e5fd09c6e16c
Šárka Flídrová <sarka.flidrova@email.cz>
2024-05-10 20:15:04 +0200

    Fixed quest vs. host

--- src/components/Episode.jsx
+++ src/components/Episode.jsx
@@ -1,10 +1,10 @@
-export const Episode = ({num, title, host}) => {
+export const Episode = ({num, title, guest}) => {
     return (
         <div className="episode">
         <div className="episode__num">{num}</div>
         <div className="episode__body">
           <div className="episode__title">{title}</div>
-          <div className="episode__guest">host: {host}</div>
+          <div className="episode__guest">host: {guest}</div>
         </div>
       </div>
     )
